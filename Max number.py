9f13bc76965fff3ceed896c10dc78556bde7f990
Abdul Shaheed Habib <ashabib001@st.ug.edu.gh>
2024-12-07 19:17:56 +0000

    Add files via upload

---
+++ Max number.py
@@ -0,0 +1,9 @@
+numbers =[5, 2, 5, 2, 2]
+for number in numbers:
+   print('x' * number)
+numbers = [2, 4, 6, 8, 10]
+max=numbers[0]
+for number in numbers:
+    if number > max:
+       max=number
+       print(max)
